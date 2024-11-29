26baf0d0b0f84ef9562c8a32cd8431986c182861
HadenHoo-web <hoquochuy99.2019@gmail.com>
2024-11-29 09:53:28 +0700

    update

--- saleapp/app/admin.py
+++ saleapp/app/admin.py
@@ -1,8 +1,9 @@
 from app import app, db
-from flask_admin import Admin
+from flask import redirect
+from flask_admin import Admin, BaseView, expose
 from flask_admin.contrib.sqla import ModelView
 from app.models import Category, Product, User, UserRole
-from flask_login import current_user
+from flask_login import current_user, logout_user
 
 
 class AuthenticatedView(ModelView):
@@ -19,6 +20,23 @@ class ProductView(AuthenticatedView):
     can_export = True
 
 
+class AuthenticatedBaseView(BaseView):
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+
+class LogoutView(AuthenticatedBaseView):
+    @expose("/")
+    def index(self):
+        logout_user()
+        return redirect("/admin")
+
+class StatsView(AuthenticatedBaseView):
+    @expose("/")
+    def index(self):
+        #...
+        return self.render("admin/stats.html")
+
 class CategoryView(AuthenticatedView):
     column_list = ['name', 'products']
 
@@ -27,3 +45,5 @@ admin = Admin(app=app, name='eCommerceApp', template_mode='bootstrap4')
 admin.add_view(CategoryView(Category, db.session))
 admin.add_view(ProductView(Product, db.session))
 admin.add_view(AuthenticatedView(User, db.session))
+admin.add_view(StatsView(name='Thống kê'))
+admin.add_view(LogoutView(name="Đăng xuất"))
