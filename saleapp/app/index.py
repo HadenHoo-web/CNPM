798fd9bf6d742f294b6d606d1bb0841347db8096
HadenHoo-web <140795485+HadenHoo-web@users.noreply.github.com>
2024-11-29 18:45:17 +0700

    update

--- saleapp/app/index.py
+++ saleapp/app/index.py
@@ -9,7 +9,6 @@ from app.models import UserRole
 
 @app.route("/")
 def index():
-    cates = dao.load_categories()
 
     page = request.args.get('page', 1)
     cate_id = request.args.get('category_id')
@@ -19,7 +18,7 @@ def index():
     page_size = app.config["PAGE_SIZE"]
     total = dao.count_products()
 
-    return render_template("index.html", categories=cates, products=prods, pages=math.ceil(total / page_size))
+    return render_template("index.html",  products=prods, pages=math.ceil(total / page_size))
 
 
 @app.route("/register", methods=['get', 'post'])
@@ -87,11 +86,21 @@ def add_to_cart():
     #session
     return jsonify(utils.stats_cart(cart))
 
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
 @app.route('/logout')
 def logout_process():
     logout_user()
     return redirect('/login')
 
+@app.context_processor
+def common_response():
+    return {
+        'categories':dao.load_categories(),
+        'cart_stats':utils.stats_cart(session.get('cart'))
+    }
 
 @login.user_loader
 def load_user(user_id):
